d2017d78dae0cf34de6a226cda9dd3df8d81acfe
JJ Merelo <jjmerelo@gmail.com>
2014-12-06 23:16:21 +0100

    Termina instrucciones

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # inspired by http://stackoverflow.com/questions/14024594/how-do-i-prevent-the-github-pages-automatic-generator-to-remove-everything-bef and http://www.cyberciti.biz/faq/sed-howto-remove-lines-paragraphs/
+# Please read README.md for instructions 
+# Might error if _layout exists
 mkdir _layouts
 sed '/id="main_content">/,/<\/section>/d' index.html > /tmp/temp
 sed 's/<hr>/<hr>\n{{content}}/' /tmp/temp > _layouts/index.html
 rm /tmp/temp
-echo "---\nlayout: index\n---\n" > index.md
+echo -e "---\nlayout: index\n---\n" > index.md
 echo "markdown: kramdown" > _config.yml
 git add _config.yml index.md _layouts/index.html
 
