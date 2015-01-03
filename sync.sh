eb55d8950ddfa34f87b30f76bc4b5d4c40a97bb8
JJ Merelo <jjmerelo@gmail.com>
2015-01-03 19:50:15 +0100

    Eliminates debug info

--- sync.sh
+++ sync.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 git_branch="$(git status | head -1)"
-echo $git_branch
 if [[ "$git_branch" =~ "master" ]]
 then
-    echo "git_branch = ${git_branch}"
     git checkout gh-pages
     git checkout master README.md
     echo -e "---\nlayout: index\n---\n" > index.md.temp
